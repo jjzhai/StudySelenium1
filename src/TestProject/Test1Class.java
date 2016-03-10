a2ea4416c726092926a1b28f5d038a2eea037b6c
jjzhai <jjzhai@lisystec.com>
2016-03-10 16:59:00 +0800

    first commit

---
+++ src/TestProject/Test1Class.java
@@ -0,0 +1,12 @@
+package TestProject;
+
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+public class Test1Class extends SeleneseTestCase{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
